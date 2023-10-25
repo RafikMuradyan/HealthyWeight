1d64f354f5401bdee43e91b6be3448a958af59d8
RafikMuradyan <muradyanrafik1@gmail.com>
2023-10-26 01:14:13 +0400

    added orm config

---
+++ ormconfig.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+
+import { SnakeNamingStrategy } from './src/snake-naming.strategy';
+
+dotenv.config();
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  namingStrategy: new SnakeNamingStrategy(),
+  subscribers: [],
+  entities: [
+    'src/modules/**/*.entity{.ts,.js}',
+    'src/modules/**/*.view-entity{.ts,.js}',
+  ],
+  migrations: ['src/migrations/*{.ts,.js}'],
+  synchronize: false,
+});
