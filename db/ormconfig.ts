c294b740f1efd8201c2317be258239886956b770
melineabrahamyan <abrahamyan.meline@gmail.com>
2024-01-05 20:35:44 +0400

    vercel

--- db/ormconfig.ts
+++ db/ormconfig.ts
@@ -3,14 +3,13 @@ require('dotenv').config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.POSTGRES_URL + '?sslmode=require',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  synchronize: true,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
-  synchronize: false,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
