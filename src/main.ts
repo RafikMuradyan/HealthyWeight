2cd8a2c2476c7a084c9cf7f8060b86433f3909d6
RafikMuradyan <muradyanrafik1@gmail.com>
2024-01-16 11:01:31 +0400

    added ValidationPipe as globalPipe

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<NestExpressApplication> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
 
+  app.useGlobalPipes(new ValidationPipe())
   app.setGlobalPrefix('api');
   app.use(helmet());
   app.use(
