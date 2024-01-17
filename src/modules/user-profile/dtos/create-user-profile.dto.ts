baa1ae559d85164adb925dafc4c35a8b578985b8
melineabrahamyan <abrahamyan.meline@gmail.com>
2024-01-17 16:57:15 +0400

    fixed max value validation

--- src/modules/user-profile/dtos/create-user-profile.dto.ts
+++ src/modules/user-profile/dtos/create-user-profile.dto.ts
@@ -1,17 +1,20 @@
-import { IsString, IsInt, IsIn, Min } from 'class-validator';
+import { IsString, IsInt, IsIn, Min, Max } from 'class-validator';
 import { Gender } from '../user-profile.enums';
 
 export class CreateUserProfileDto {
   @IsInt({ message: 'Age must be an integer' })
   @Min(1, { message: 'Age must be positive number' })
+  @Max(100, { message: 'Age must be less then or equal to 100' })
   age: number;
 
   @IsInt({ message: 'Height must be an integer' })
   @Min(1, { message: 'Height must be positive number' })
+  @Max(260, { message: 'Height must be less then or equal to 260' })
   height: number;
 
   @IsInt({ message: 'Weight must be an integer' })
   @Min(1, { message: 'Weight must be positive number' })
+  @Max(400, { message: 'Weight must be less then or equal to 400' })
   weight: number;
 
   @IsString()
--- src/modules/user-profile/dtos/user-profile-data.dto.ts
+++ src/modules/user-profile/dtos/user-profile-data.dto.ts
@@ -1,13 +1,15 @@
-import { IsString, IsInt, IsIn, Min } from 'class-validator';
+import { IsString, IsInt, IsIn, Min, Max } from 'class-validator';
 import { Gender } from '../user-profile.enums';
 
 export class UserProfileDataDto {
   @IsInt({ message: 'Age must be an integer' })
   @Min(1, { message: 'Age must be positive number' })
+  @Max(100, { message: 'Age must be less then or equal to 100' })
   age: number;
 
   @IsInt({ message: 'Height must be an integer' })
   @Min(1, { message: 'Height must be positive number' })
+  @Max(260, { message: 'Height must be less then or equal to 260' })
   height: number;
 
   @IsString()
