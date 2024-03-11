1e09188b4d9dbba00a3fd7d725e50717b8391077
RafikMuradyan <muradyanrafik1@gmail.com>
2024-03-11 11:51:01 +0400

    refactored entities

--- src/modules/user-profile/user-profile.entity.ts
+++ src/modules/user-profile/user-profile.entity.ts
@@ -2,29 +2,17 @@ import { Entity, Column } from 'typeorm';
 import { Gender, WeightStatus } from './user-profile.enums';
 import { AbstractEntity } from '../../common/abstract.entity';
 
-@Entity()
+@Entity({ name: 'user_profile' })
 export class UserProfile extends AbstractEntity {
-  @Column({
-    type: 'smallint',
-    unsigned: true,
-  })
+  @Column({ type: 'smallint', unsigned: true })
   age: number;
 
-  @Column({
-    type: 'smallint',
-    unsigned: true,
-  })
+  @Column({ type: 'smallint', unsigned: true })
   height: number;
 
-  @Column({
-    type: 'enum',
-    enum: Gender,
-  })
+  @Column({ type: 'enum', enum: Gender })
   gender: string;
 
-  @Column({
-    type: 'enum',
-    enum: WeightStatus,
-  })
+  @Column({ type: 'enum', enum: WeightStatus })
   weightStatus: WeightStatus;
 }
