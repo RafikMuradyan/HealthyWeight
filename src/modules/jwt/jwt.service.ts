dbe349e8896218aa9b0af743be030bdb2d29ba5a
RafikMuradyan <muradyanrafik1@gmail.com>
2024-04-08 23:50:29 +0400

    added genereic type for jwt payload

--- src/modules/jwt/jwt.service.ts
+++ src/modules/jwt/jwt.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
 import { ITokenPayload } from '../notification/interfaces';
+import { JwtPayload } from './types';
 
 @Injectable()
 export class JwtService {
-  public generateToken(payload: ITokenPayload): string {
+  public generateToken<T extends JwtPayload>(payload: T): string {
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
       expiresIn: '7d',
     });
