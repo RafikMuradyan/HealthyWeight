4e600e4facb025fc12b979d7e10af0d962a4ec48
RafikMuradyan <muradyanrafik1@gmail.com>
2024-04-15 01:06:41 +0400

    added log to check token

--- src/configs/telegram.setup.ts
+++ src/configs/telegram.setup.ts
@@ -1,5 +1,7 @@
 import { Telegraf } from 'telegraf';
 
+const a = process.env.TELEGRAM_BOT_TOKEN;
+console.log(a, '11111111111111111111111');
 export const telegramBot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 export const telegramAdmins: Array<string> = [
   process.env.TELEGRAM_ADMIN1,
