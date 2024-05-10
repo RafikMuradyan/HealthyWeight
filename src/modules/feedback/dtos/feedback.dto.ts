e349a960206f938ff51c501d22f15913b91d4907
RafikMuradyan <muradyanrafik1@gmail.com>
2024-05-10 17:27:47 +0400

    added neccessary fields to feedback dto

--- src/modules/feedback/dtos/feedback.dto.ts
+++ src/modules/feedback/dtos/feedback.dto.ts
@@ -1,11 +1,15 @@
 import { Feedback } from '../feedback.entity';
 
 export class FeedbackDto {
+  firstName: string;
+  lastName: string;
   fullName: string;
   content: string;
   isConfirmed: boolean;
 
   constructor(feedback: Feedback) {
+    this.firstName = feedback.firstName;
+    this.lastName = feedback.lastName;
     this.fullName = feedback.fullName;
     this.content = feedback.content;
     this.isConfirmed = feedback.isConfirmed;
